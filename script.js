2ceaff22ca8dc8772f24485431fef82418dc24d9
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-10 15:52:21 +0600

    updates

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ window.addEventListener("load", () => {
         navigator.geolocation.getCurrentPosition(positon => {
             long = positon.coords.longitude;
             lat = positon.coords.latitude;
+            
+
+            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            
         })
     }
 })
