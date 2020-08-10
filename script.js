26d4538ecaf9802b70338cd648cdb14fd9ad6c67
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-10 17:51:09 +0600

    script

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 window.addEventListener("load", () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-des");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
 
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(positon => {
-            long = positon.coords.longitude;
-            lat = positon.coords.latitude;
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            console.log(lat , long);
             
-
-            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
-
+        
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+            
             fetch(api)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-            })
-            
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    // const {temperature , summary} = data.currently;
+                    // temperatureDegree.textContent = temperature;
+                })
         })
-    }
-})
+        
+    };
+});
