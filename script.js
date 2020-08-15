f3c5cfff17b154f8ab7749729644d44cecac3ac5
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-15 12:19:12 +0600

    update

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+//ON window load function will be execute
 window.addEventListener("load", () => {
     let temperatureDegree = document.querySelector(".temperature-degree");
     let cityName = document.getElementById("cityName");
     let weatherStatus = document.getElementById("status");
     let weatherIcon = document.getElementById("weatherIcon");
 
+    //get input value and submit button for getting all formation
     document.getElementById("submit-id").addEventListener("click", function(){
         const findValue = document.getElementById("get-value").value;
         getResults(findValue);
         
     })
 
+    //API call
     function getResults(valueCity) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${valueCity}&units=metric&APPID=6f0afe92ff5231fed4df9aeac089587d`)
             .then(weather => {
